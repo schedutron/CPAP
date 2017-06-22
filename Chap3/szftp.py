1ab40e6ff835bce923f4ddb71505008cd7e84870
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-06-22 13:22:20 +0530

    Complete Exercise 3-10; zip functionality in FTP client

---
+++ Chap3/szftp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import subprocess, sys, os, shutil
+
+for i in range(len(sys.argv)):
+    if '-z' == sys.argv[i]:
+        msg = sys.argv[1:i]
+        name = sys.argv[i+1] #name of the zip file
+        os.mkdir(name)
+        os.chdir(name)
+        subprocess.call(['../srpmftp.py'] + msg) #to do the download
+        os.chdir('..')
+        shutil.make_archive(name, 'zip', '.', name)
+        shutil.rmtree(name)
+        break
+else:
+    msg = sys.argv[1:]
+    subprocess.call(['./srpmftp.py']+msg)

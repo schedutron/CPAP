7292c95ed16af062ee7df0f4ba73cace99a11fa2
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-06-23 17:28:01 +0530

    Complete Exercise 3-5; smart FTP GUI (download) client, plenty of room for improvement

--- Chap3/smart_FTP_clnt_GUI.py
+++ Chap3/smart_FTP_clnt_GUI.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
+from kivy.uix.button import Button
 
 #this is just a download client, upload functionality absent
 from ftplib import FTP, error_perm
@@ -19,10 +20,21 @@ class Client(BoxLayout):
     dir_name_text_input = ObjectProperty()
     dir_contents = ObjectProperty()
     status = ObjectProperty()
+    the_buttons = ObjectProperty()
+    history = ObjectProperty()
+    bookmarks = ObjectProperty()
+    downloads = ObjectProperty()
+    bookmark_btn = None #the button which asks to bookmark a location
     f = None #FTP Object
     contents = [] #to store directory data
 
     def get(self):
+        #removes the bookmark button if it exists
+        if self.bookmark_btn:
+            self.the_buttons.remove_widget(self.bookmark_btn)
+            self.bookmark_btn = None
+
+        #reads host name and directory name from input
         hname = self.host_name_text_input.text
         dname = self.dir_name_text_input.text
         #login to host
@@ -33,6 +45,10 @@ class Client(BoxLayout):
             except:
                 self.host_name_text_input.text = "Cannot connect. Try again."
                 return
+
+            history_dict['history'].append('%s - %s' % (self.f.host, ctime()))
+            self.history.values = history_dict['history'] #updates the history list for display
+            self.update_db()
 
         #cd to the directory
         try:
@@ -57,6 +73,18 @@ class Client(BoxLayout):
 
         self.dir_contents._trigger_reset_populate()
 
+        self.location = "%s" % self.f.host + self.f.pwd()
+        if self.location not in history_dict['bookmarks']:
+            self.bookmark_btn = Button(text="Bookmark this location?")
+            self.the_buttons.add_widget(self.bookmark_btn)
+            self.bookmark_btn.bind(on_press=self.add_bookmark)
+
+    def add_bookmark(self, *ignore): #ignore the callback arguments passed
+        history_dict["bookmarks"].append(self.location)
+        self.bookmarks.values = history_dict["bookmarks"]
+        self.update_db()
+        self.the_buttons.remove_widget(self.bookmark_btn) #updates the bookmarks list for displays
+
     def download(self):
         #if items are selected
         if self.dir_contents.adapter.selection:
@@ -68,23 +96,33 @@ class Client(BoxLayout):
             for item in self.dir_contents.adapter.selection:
                 stuff = item.text
                 self.status.text = "Downloading %s" % stuff
-                print self.update_status()
                 with open(stuff, 'wb') as content:
                     #callback = Callback(content, size)
                     self.f.retrbinary("RETR %s" % stuff, content.write, 1024)
-                    #history["downloads"].append("%s%s%s - %s" % (chunks[1], dirname, item, ctime()))
+                    history_dict["downloads"].append("%s%s%s - %s" % (self.f.host, self.f.pwd(), stuff, ctime()))
+                    self.downloads.values = history_dict["downloads"] #updates the downloads list for display
                 self.status.text = "%s downloaded!" % stuff
             os.chdir('..')
-            #self.status.text = "All download(s) complete!"
+            self.update_db()
+            self.status.text = "All download(s) complete!"
 
     def behind(self):
         #move one step back in the directory tree
         try:
             self.f.cwd('..')
-            self.dir_name_text_input.text = '/' + '/'.join(self.dir_name_text_input.text.split('/')[:-1])
+            self.dir_name_text_input.text = '/'.join(self.dir_name_text_input.text.split('/')[:-1])
+            if not self.dir_name_text_input.text:
+                self.dir_name_text_input.text = '/'
             self.get()
         except:
             self.dir_name_text_input.text = 'Unable to change directory.'
+
+    def update_db(self): #writes to the pickle file
+        with open('history.pkl', 'wb') as write_file:
+            pickle.dump(history_dict, write_file)
+
+with open('history.pkl', 'rb') as h:
+    history_dict = pickle.load(h)
 
 class FTPClientApp(App):
     def build(self):

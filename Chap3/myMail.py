58c1056aa0855fd398b39dd84f939d31348a57b3
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-07 23:56:39 +0530

    Currently broken, fetches old email

--- Chap3/myMail.py
+++ Chap3/myMail.py
@@ -2,37 +2,57 @@
 
 from smtplib import SMTP
 from poplib import POP3_SSL
+from secret import *
 from time import sleep
 
 SMTPSVR = 'smtp.gmail.com'
 POPSVR = 'pop.gmail.com'
 
-who = 'ImNotGonnaReveal@myEmail.com'
-body = '''
-From: %(who)s
+who = MAILBOX + '@gmail.com'
+body = """From: %(who)s
 To: %(who)s
 Subject: test msg
 
-Hello, World!''' % {'who': who}
+Hello, World!""" % {'who': who}
 
-origBody = body[body.index('')+1:].split('\n')
+body_list = body.split('\n')
+breaker = body_list.index('')
+origHeaders = body_list[:breaker] # List
+origBody = ''.join(body_list[breaker+1:])
+print(origBody)
 sendSvr = SMTP(SMTPSVR)
 sendSvr.starttls()
-sendSvr.login(who, 'ImNotGonnaRevealMyPassword')
+sendSvr.login(who, PASSWD)
 errs = sendSvr.sendmail(who, [who], body)
 sendSvr.quit()
 assert len(errs) == 0, errs
-sleep(5) #wait for mail to be delivered
+sleep(10) #wait for mail to be delivered
 
 recvSvr = POP3_SSL(POPSVR)
 recvSvr.user(who)
-recvSvr.pass_('ImNotGonnaRevealMyPassword')
-rsp, msg, siz = recvSvr.retr(recvSvr.stat()[0])
+recvSvr.pass_(PASSWD)
+rsp, msg, siz = recvSvr.retr(recvSvr.stat()[-1])
 #strip headers and compare to orig msg
 #print(msg)
+#print("Full message: ")
+
 recvBody = [text.decode('unicode_escape') for text in msg] #to convert the recieved binary strings in msg
-recvBody = recvBody[recvBody.index('')+1:]
-#print('recvBody:\n%s\n' % recvBody)
-#print('origBody:\n%s\n' % origBody)
+breaker = recvBody.index('')
+for text in recvBody:
+    print(text)
+#for item in recvBody:
+#    print(item)
+recvHeaders = recvBody[:breaker]
+# Loop used to ignore new headers.
+for i in range(len(recvHeaders)):
+    if recvHeaders[i].startswith('From:'):
+        break
+recvHeaders = recvHeaders[i:i+3]
+recvBody = recvBody[breaker+1:]
+print('recvBody:\n%s\n' % recvBody)
+print('origBody:\n%s\n' % origBody)
 recvSvr.quit() #important! commits pending changes!
+#print('recvHeaders:\n%s\n' % recvHeaders)
+#print('origHeaders:\n%s\n' % origHeaders)
+#Sassert recvHeaders == origHeaders
 assert recvBody == origBody #assert identical

2e231156369fe59b5024830ddccf54509ee30486
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-08 17:08:10 +0530

    Complete Exercise 3-24: Add IMAP support

--- Chap3/myMail.py
+++ Chap3/myMail.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
+from imaplib import IMAP4_SSL
 from smtplib import SMTP_SSL, SMTPAuthenticationError
 from poplib import POP3_SSL, error_proto
 from secret import *
 from socket import gaierror
 from time import sleep
 SMTPSVR = 'smtp.gmail.com'
+IMAPSVR = 'imap.gmail.com'
 POPSVR = 'pop.gmail.com'
 
 who = MAILBOX + '@gmail.com'
@@ -36,10 +37,30 @@ sendSvr.quit()
 assert len(errs) == 0, errs
 sleep(10) #wait for mail to be delivered
 
+# IMAP stuff below.
+try:
+    recvSvr = IMAP4_SSL(IMAPSVR, 993)
+except gaierror:
+    print("Can't connect to %s." % IMAPSVR)
+    exit()
+try:
+    recvSvr.login(who, PASSWD)
+except imaplib.error as e:
+    print(str(e)[2:-1])  # This may be a stupid thing to do.
+    exit()
+rsp, msgs = recvSvr.select('INBOX', True)
+try:
+    rsp, data = recvSvr.fetch(msgs[0], '(RFC822)')
+except imaplib.error as e:
+    print(e)
+    exit()
+recvBody = data[0][1].decode('unicode_escape').split('\r\n')[:-1] # [:-1] removes the empty string at last.
+
+"""
 try:
     recvSvr = POP3_SSL(POPSVR, 995)
 except gaierror:
-    print("Can't connect to %s." %POPSVR)
+    print("Can't connect to %s." % POPSVR)
     exit()
 # No need of the following try-except block.
 # This error would be already catched in SMTP part.
@@ -56,10 +77,10 @@ rsp, msg, siz = recvSvr.retr(recvSvr.stat()[0])
 #print(msg)
 #print("Full message: ")
 recvSvr.quit() #important! commits pending changes!
+
 recvBody = [text.decode('unicode_escape') for text in msg] #to convert the recieved binary strings in msg
+"""
 breaker = recvBody.index('')
-for text in recvBody:
-    print(text)
 #for item in recvBody:
 #    print(item)
 recvHeaders = recvBody[:breaker]
@@ -71,7 +92,7 @@ recvHeaders = recvHeaders[i:i+3]
 recvBody = '\n'.join(recvBody[breaker+1:])
 #print('recvBody:\n%s\n' % recvBody)
 #print('origBody:\n%s\n' % origBody)
-print('recvHeaders:\n%s\n' % recvHeaders)
-print('origHeaders:\n%s\n' % origHeaders)
-#assert recvHeaders == origHeaders
+#print('recvHeaders:\n%s\n' % recvHeaders)
+#print('origHeaders:\n%s\n' % origHeaders)
+assert recvHeaders == origHeaders
 assert recvBody == origBody #assert identical

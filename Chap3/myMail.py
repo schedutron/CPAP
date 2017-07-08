209c542a2785c930a82e476c3fced9deb5b4c8ea
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-08 17:17:42 +0530

    Add choice of receiving protocols for extra credit of Exercise 3-24

--- Chap3/myMail.py
+++ Chap3/myMail.py
@@ -35,51 +35,56 @@ except SMTPAuthenticationError:
 errs = sendSvr.sendmail(who, [who], body)
 sendSvr.quit()
 assert len(errs) == 0, errs
+choice = input("Mail sent. Which protocol do you want to use for receiving it? (POP / IMAP)? ")
+if not (choice == "POP" or choice =="IMAP"):
+    print("Please enter a valid choice.")
+    exit()
 sleep(10) #wait for mail to be delivered
+if choice == "IMAP":
+    # IMAP stuff below.
+    try:
+        recvSvr = IMAP4_SSL(IMAPSVR, 993)
+    except gaierror:
+        print("Can't connect to %s." % IMAPSVR)
+        exit()
+    try:
+        recvSvr.login(who, PASSWD)
+    except imaplib.error as e:
+        print(str(e)[2:-1])  # This may be a stupid thing to do.
+        exit()
+    rsp, msgs = recvSvr.select('INBOX', True)
+    try:
+        rsp, data = recvSvr.fetch(msgs[0], '(RFC822)')
+    except imaplib.error as e:
+        print(e)
+        exit()
+    recvBody = data[0][1].decode('unicode_escape').split('\r\n')[:-1] # [:-1] removes the empty string at last.
 
-# IMAP stuff below.
-try:
-    recvSvr = IMAP4_SSL(IMAPSVR, 993)
-except gaierror:
-    print("Can't connect to %s." % IMAPSVR)
-    exit()
-try:
-    recvSvr.login(who, PASSWD)
-except imaplib.error as e:
-    print(str(e)[2:-1])  # This may be a stupid thing to do.
-    exit()
-rsp, msgs = recvSvr.select('INBOX', True)
-try:
-    rsp, data = recvSvr.fetch(msgs[0], '(RFC822)')
-except imaplib.error as e:
-    print(e)
-    exit()
-recvBody = data[0][1].decode('unicode_escape').split('\r\n')[:-1] # [:-1] removes the empty string at last.
+else:
+    # POP stuff below.
+    try:
+        recvSvr = POP3_SSL(POPSVR, 995)
+    except gaierror:
+        print("Can't connect to %s." % POPSVR)
+        exit()
+    # No need of the following try-except block.
+    # This error would be already catched in SMTP part.
+    # It's present to signify that invalid credential error has been taken care of.
+    try:
+        recvSvr.user(who)
+        recvSvr.pass_(PASSWD)
+    except error_proto as e:
+        print("For POP:", end=" ")
+        print(str(e)[2:-1]) # This may be a stupid thing to do.
+        exit()
+    rsp, msg, siz = recvSvr.retr(recvSvr.stat()[0])
+    #strip headers and compare to orig msg
+    #print(msg)
+    #print("Full message: ")
+    recvSvr.quit() #important! commits pending changes!
 
-"""
-try:
-    recvSvr = POP3_SSL(POPSVR, 995)
-except gaierror:
-    print("Can't connect to %s." % POPSVR)
-    exit()
-# No need of the following try-except block.
-# This error would be already catched in SMTP part.
-# It's present to signify that invalid credential error has been taken care of.
-try:
-    recvSvr.user(who)
-    recvSvr.pass_(PASSWD)
-except error_proto as e:
-    print("For POP:", end=" ")
-    print(str(e)[2:-1]) # This may be a stupid thing to do.
-    exit()
-rsp, msg, siz = recvSvr.retr(recvSvr.stat()[0])
-#strip headers and compare to orig msg
-#print(msg)
-#print("Full message: ")
-recvSvr.quit() #important! commits pending changes!
+    recvBody = [text.decode('unicode_escape') for text in msg] #to convert the recieved binary strings in msg
 
-recvBody = [text.decode('unicode_escape') for text in msg] #to convert the recieved binary strings in msg
-"""
 breaker = recvBody.index('')
 #for item in recvBody:
 #    print(item)

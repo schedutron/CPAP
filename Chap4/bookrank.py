d2ebb52b088ec644ae3c3dc248200182a09ab3a8
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-07-22 19:56:41 +0530

    Complete Example 4-9: Book rankings screenscraper; create requirements file

---
+++ Chap4/bookrank.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from atexit import register
+from re import compile
+from threading import Thread
+from time import ctime
+from urllib.request import urlopen
+
+REGEX = compile(r"#([\d,]+) in Books ")
+AMZN = "http://amazon.com/dp/"
+ISBNs = {
+    '0132269937': "Core Python Programming",
+    '0132356139': "Python Web Development with Django",
+    '0137143419': "Python Fundamentals"
+}
+
+def get_ranking(isbn):
+    page = urlopen("%s%s" % (AMZN, isbn))  # Or str.format()
+    data = page.read().decode()
+    page.close()
+    return REGEX.findall(data)[0]
+
+def _show_ranking(isbn):
+    print("- %r ranked %s" % (ISBNs[isbn], get_ranking(isbn)))
+
+def _main():
+    print("At", ctime(), "on Amazon...")
+    for isbn in ISBNs:
+        _show_ranking(isbn)
+
+@register
+def _atexit():
+    print("all DONE at:", ctime())
+
+if __name__ == "__main__":
+    _main()

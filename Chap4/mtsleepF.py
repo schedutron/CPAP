1d8cc2f45c4a300dc91403d5cae70fc13c7400a4
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-08-01 00:11:33 +0530

    Towards understanding need for locks

---
+++ Chap4/mtsleepF.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from atexit import register
+from random import randrange
+from threading import Thread, currentThread
+from time import ctime, sleep
+
+class CleanOutputSet(set):
+    def __str__(self):
+        return ", ".join(x for x in self)
+
+
+loops = (randrange(2, 5) for x in range(randrange(3, 7)))
+remaining = CleanOutputSet()
+
+def loop(nsec):
+    myname = currentThread().name
+    remaining.add(myname)
+    print("[%s] Started %s" % (ctime(), myname))
+    sleep(nsec)
+    remaining.remove(myname)
+    print("[%s] Completed %s (%d secs)" % (ctime(), myname, nsec))
+    print("    (remaining: %s)" % (remaining or "NONE"))
+
+
+def _main():
+    for pause in loops:
+        Thread(target=loop, args=(pause,)).start()
+
+
+@register
+def _atexit():
+    print("all DONE at:", ctime())
+
+
+if __name__ == "__main__":
+    _main()

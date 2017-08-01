e226858da7b03f114f6b6de62104387689d3ca80
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-08-01 23:29:43 +0530

    Add locks for clean output

--- Chap4/mtsleepF.py
+++ Chap4/mtsleepF.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 from atexit import register
 from random import randrange
-from threading import Thread, currentThread
+from threading import currentThread, Lock, Thread
 from time import ctime, sleep
 
 class CleanOutputSet(set):
     def __str__(self):
         return ", ".join(x for x in self)
 
-
+lock = Lock()
 loops = (randrange(2, 5) for x in range(randrange(3, 7)))
 remaining = CleanOutputSet()
 
 def loop(nsec):
     myname = currentThread().name
+    lock.acquire()
     remaining.add(myname)
     print("[%s] Started %s" % (ctime(), myname))
+    lock.release()
     sleep(nsec)
+    lock.acquire()
     remaining.remove(myname)
     print("[%s] Completed %s (%d secs)" % (ctime(), myname, nsec))
     print("    (remaining: %s)" % (remaining or "NONE"))
+    lock.release()
 
 
 def _main():

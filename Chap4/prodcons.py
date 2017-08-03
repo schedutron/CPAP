a6ef7eaf6811a1b19a473eba790604056ff1d036
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-08-03 22:22:33 +0530

    Complete Example 4-12: Producer-Consumer Problem

---
+++ Chap4/prodcons.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from myThread import MyThread
+from queue import Queue
+from random import randint
+from time import sleep
+
+
+def writeQ(queue):
+    print("producing object for Q...", end=" ")
+    queue.put("xxx", 1)
+    print("size now:", queue.qsize())
+
+
+def readQ(queue):
+    val = queue.get(1)
+    print("consumed object from Q... size now", queue.qsize())
+
+
+def writer(queue, loops):
+    for i in range(loops):
+        writeQ(queue)
+        sleep(randint(1, 3))
+
+
+def reader(queue, loops):
+    for i in range(loops):
+        readQ(queue)
+        sleep(randint(2, 5))
+
+
+funcs = [writer, reader]
+nfuncs = range(len(funcs))
+
+
+def main():
+    nloops = randint(2, 5)
+    q = Queue(32)
+
+    threads = []
+    for i in nfuncs:
+        t = MyThread(funcs[i], (q, nloops), funcs[i].__name__)
+        threads.append(t)
+
+    for i in nfuncs:
+        threads[i].start()
+
+    for i in nfuncs:
+        threads[i].join()
+
+    print("all DONE")
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -8,7 +8,9 @@
 * [Chapter 4: Multithreaded Programming][chap4]
     * [Example 4-10: Locks and More Randomness (mtsleepF.py)][4-10]
     * [Example 4-11: Candy Vending Machines and Semaphores (candy.py)][4-11]
+    * [Example 4-12: Producer-Consumer Problem (prodcons.py)][4-12]
 
 [chap4]: /Chap4
 [4-10]: /Chap4/mtsleepF.py
 [4-11]: /Chap4/candy.py
+[4-12]: /Chap4/prodcons.py

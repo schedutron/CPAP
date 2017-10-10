67bc26b323b8a3e10c500e8233f76ba9429a591f
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-10-10 23:47:46 +0530

    Example 6-1; main() remaining

--- Chap6/ushuffle_dbU.py
+++ Chap6/ushuffle_dbU.py
@@ -99,3 +99,70 @@ def connect(db, DBNAME):
         return None
     return cxn
 
+
+def create(cur):
+    try:
+        cur.execute('''
+        CREATE TABLE users(
+            login VARCHAR(%d),
+            userid INTEGER,
+            projid INTEGER)
+            ''' % NAMELEN)
+    except DB_EXC.OperationalError:
+        drop(cur)
+        create(cur)
+
+
+drop = lambda cur: cur.execute('DROP TABLE users')
+
+NAMES = (
+    ('aaron', 8312), ('angela', 7603), ('dave', 7306),
+    ('davina', 7902), ('elloit', 7911), ('ernie', 7410),
+    ('jess', 7912), ('jim', 7512), ('larry', 7311),
+    ('leslie', 7808), ('mellissa', 8602), ('pat', 7711),
+    ('serena', 7003), ('stan', 7607), ('faye', 6812),
+    ('amy', 7209), ('mona', 7404), ('jennifer', 7608),
+)
+
+
+def randName():
+    pick = set(NAMES)
+    while pick:
+        yield pick.pop()
+
+
+def insert(cur, db):
+    if db == 'sqlite':
+        cur.executemany("INSERT INTO users VALUES (?, ?, ?)",
+        [(who, uid, rand(1, 5)) for who, uid in randName()])
+    elif db == 'gadfly':
+        for who, uid in randName():
+            cur.execute("INSERT INTO users VALUES(?, ?, ?)", (who, uid, rand(1, 5)))
+    elif db == 'mysql':
+        cur.executemany("INSERT INTO users VALUES(%s, %s, %s)",
+        [(who, uid, rand(1, 5)) for who, uid in randName()])
+
+getRC = lambda cur: cur.rowcount if hasattr(cur, 'rowcount') else -1
+
+
+def update(cur):
+    fr = rand(1, 5)
+    to = rand(1, 5)
+    cur.execute("UPDATE users SET projid=%d WHERE projid=%d" % (to, fr))
+    return fr, to, getRC(cur)
+
+
+def delete(cur):
+    rm = rand(1, 5)
+    cur.execute("DELETE FROM users WHERE projid=%d", %rm)
+    return rm, getRC(cur)
+
+
+def dbDump(cur):
+    cur.execute('SELECT * FROM users')
+    printf('\n%s' % ''.join(map(cformat, FIELDS)))
+    for data in cur.fetchall():
+        printf(''.join(map(tformat, data)))
+
+
+def main():
